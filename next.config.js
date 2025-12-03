1708b568fae975384b67b84d4fb72df0c36b6dc4
Ezequias Rocha <ezequias.rocha@gmail.com>
2025-12-03 14:13:27 -0300

    Updates pra subir projeto.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-}
+
+  // ← ESSA PARTE RESOLVE O ERRO DO TURBOPACK
+  turbopack: {
+    root: '.', // Força o Next.js a usar a pasta atual como raiz do projeto
+  },
+};
+
+module.exports = nextConfig;
