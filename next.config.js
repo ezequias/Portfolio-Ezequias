e9ec24ae2e296b443f568c55bdf4c5c95c33bd7a
Ezequias Rocha <ezequias.rocha@gmail.com>
2025-12-03 15:36:47 -0300

    Deploy final

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+
+  // ESSAS DUAS LINHAS SÃO OBRIGATÓRIAS
   basePath: '/Portfolio-Ezequias',
   assetPrefix: '/Portfolio-Ezequias',
+
   trailingSlash: true,
   images: {
     unoptimized: true,
